8234f1318ea4c880235b8c706db33939ad11574a
raniyui93 <raniyui93@gmail.com>
2025-03-23 16:27:32 -0600

    code refactoring

--- src/main/java/com/quiz/controller/QuizController.java
+++ src/main/java/com/quiz/controller/QuizController.java
@@ -1,27 +1,25 @@
 package com.quiz.controller;
 
-
-import com.quiz.repository.QuizProgramRepository;
 import com.quiz.entity.QuizQuestions;
 import com.quiz.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-@CrossOrigin(origins = {"http://localhost:3000/", "*"})
+@CrossOrigin(origins = {"*"})
 @RestController
+@RequestMapping("/quiz")
 public class QuizController {
 
     @Autowired
     QuizService quizService;
 
-    @GetMapping(path = "linkQuiz")
+    @GetMapping(path = "/questions")
     public List<QuizQuestions> getQuestions() {
         return quizService.getQuestions();
     }
 
-    @PostMapping(consumes = "application/json" , path = "submitAnswer")
+    @PostMapping(consumes = "application/json" , path = "/answers")
     public Integer submitAnswer(@RequestBody List<QuizQuestions> answers){
         return quizService.verifyAnswers(answers);
     }
